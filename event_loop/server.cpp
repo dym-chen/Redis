254f89d104a7d1ea500166ec7c8c13ada629fbb1
dym-chen <danielymchen@gmail.com>
2024-09-04 20:06:55 -0400

    progress on event loop

--- event_loop/server.cpp
+++ event_loop/server.cpp
@@ -115,6 +115,90 @@ static int32_t one_request (int connfd) {
     return write_all(connfd, wbuf, 4 + len);
 }
 
+static void state_req(Conn *conn) {
+    while (try_fill_buffer(conn)) {}
+}
+
+static bool try_fill_buffer(Conn *conn) {
+    // try filling the buffer
+    assert(conn -> rbuf_size < sizeof(conn -> rbuf));
+
+    ssize_t rv = 0;
+    do {
+        size_t cap = sizeof(conn->rbuf) - conn->rbuf_size;
+        rv = read(conn->fd, &conn->rbuf[conn->rbuf_size], cap);
+    } while (rv < 0 && errno == EINTR);
+
+    if (rv < 0 && errno == EAGAIN) {
+        return false;
+    }
+
+    if (rv < 0) {
+        msg("read() error");
+        conn->state = STATE_END;
+        return false;
+    }
+
+    if (rv == 0) {
+        if (conn->rbuf_size > 0) {
+            msg("unexpected EOF");
+        } else {
+            msg("EOF");
+        }
+        conn->state = STATE_END;
+        return false;
+    }
+
+      conn->rbuf_size += (size_t)rv;
+    assert(conn->rbuf_size <= sizeof(conn->rbuf));
+
+    // Try to process requests one by one.
+    // Why is there a loop? Please read the explanation of "pipelining".
+    while (try_one_request(conn)) {}
+    return (conn->state == STATE_REQ);
+}
+
+static void connection_io(Conn *conn) {
+    if (conn->state == STATE_REQ) {
+        state_req(conn);
+    } else if (conn->state == STATE_RES) {
+        state_res(conn);
+    } else {
+        assert(0);  // not expected
+    }
+}
+
+static void conn_put(std::vector<Conn *> &fd2conn, struct Conn *conn) {
+    if (fd2conn.size() <= (size_t)conn->fd) {
+        fd2conn.resize(conn->fd + 1);
+    }
+    fd2conn[conn->fd] = conn;
+}
+
+static int32_t accept_new_conn(std::vector<Conn *> &fd2conn, int fd) {
+    // accept connection
+    struct sockaddr_in client_addr = {};
+    socklen_t socklen = sizeof(client_addr);
+    int connfd = accept(fd, (struct sockaddr *) &client_addr, &socklen);
+
+    fd_set_nb(connfd);
+    
+    struct Conn *conn = (struct Conn*) malloc(sizeof(struct Conn));
+
+    if (!conn) {
+        close(connfd);
+        return -1;
+    }
+
+    conn -> fd = connfd;
+    conn -> state = STATE_REQ;
+    conn -> rbuf_size = 0;
+    conn -> wbuf_size = 0;
+    conn -> wbuf_sent = 0;
+    conn_put(fd2conn, conn);
+    return 0;
+}
+
 int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     
@@ -184,6 +268,12 @@ int main() {
                 }
             }
         }
+
+        // try to accept a new connection if the listening fd is active
+        if (poll_args[0].revents) {
+            (void)accept_new_conn(fd2conn, fd);
+        }
         
+        return 0;        
     }
 }
